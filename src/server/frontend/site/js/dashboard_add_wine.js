101b3125b6e70e2aef34c31998bea81a66c926a8
rom.dll <pro.rafael.roma@gmail.com>
2024-12-13 22:41:15 +0100

    Wine creation interface + suggestions

--- src/server/frontend/site/js/dashboard_add_wine.js
+++ src/server/frontend/site/js/dashboard_add_wine.js
@@ -1,29 +1,31 @@
-function addWine() {
-    showModal();
-}
-
 const addWineModal = document.getElementById('addWineModal');
 const closeModal = document.getElementById('closeModal');
 const createByHandBtn = document.getElementById('createByHandBtn');
 const scanBottleBtn = document.getElementById('scanBottleBtn');
+let activeInput = null;
+let suggestionList = null
 
-function showModal() {
+function addWine() {
+    showWineAddOptionsModal();
+}
+
+function showWineAddOptionsModal() {
     addWineModal.style.display = 'flex';
     addWineModal.classList.add('show');
 }
 
-function hideModal() {
+function hideWineAddOptionsModal() {
     addWineModal.classList.remove('show');
     setTimeout(() => {
         addWineModal.style.display = 'none';
     }, 300); 
 }
 
-closeModal.addEventListener('click', hideModal);
+closeModal.addEventListener('click', hideWineAddOptionsModal);
 
 createByHandBtn.addEventListener('click', function () {
-    hideModal();
-    openModal();
+    hideWineAddOptionsModal();
+    openHardCreationModal();
 });
 
 scanBottleBtn.addEventListener('click', function () {
@@ -32,55 +34,127 @@ scanBottleBtn.addEventListener('click', function () {
 
 window.addEventListener('click', function (event) {
     if (event.target === addWineModal) {
-        hideModal();
+        hideWineAddOptionsModal();
+    }
+
+    if (event.target !== activeInput && suggestionList) {
+        suggestionList.style.display = 'none';
     }
 });
 
-function openModal() {
+function openHardCreationModal() {
     document.getElementById("addHardWineModal").style.display = "flex";
+    document.getElementById("addHardWineModal").classList.add('show');
 }
 
-// Close modal
-function closeModalFunc() {
-    document.getElementById("addHardWineModal").style.display = "none";
+function closeHardCreationModal() {
+    document.getElementById("addHardWineModal").classList.remove('show');
+    setTimeout(() => {
+        document.getElementById("addHardWineModal").style.display = 'none';
+    }, 300); 
 }
 
 async function fetchDomains(query) {
-    const domains = ['Domain A', 'Domain B', 'Domain C'];
-    const suggestions = domains.filter(domain => domain.toLowerCase().includes(query.toLowerCase()));
+    if (activeInput !== document.getElementById("domainInput") && suggestionList) {
+        suggestionList.style.display = 'none';
+    }
 
-    updateSuggestions('domainSuggestions', suggestions);
+    const domains = ['Domain A', 'Domain B', 'Domain C'];
+    const suggestions = filterSuggestions(domains, query);
+
+    updateSuggestions('domainSuggestions', suggestions, query);
+    setActiveInput(document.getElementById("domainInput"));
 }
 
 async function fetchRegions(query) {
-    const regions = ['Region A', 'Region B', 'Region C'];
-    const suggestions = regions.filter(region => region.toLowerCase().includes(query.toLowerCase()));
+    if (activeInput !== document.getElementById("regionInput") && suggestionList) {
+        suggestionList.style.display = 'none';
+    }
 
-    updateSuggestions('regionSuggestions', suggestions);
+    const regions = ['Region A', 'Region B', 'Region C'];
+    const suggestions = filterSuggestions(regions, query);
+
+    updateSuggestions('regionSuggestions', suggestions, query);
+    setActiveInput(document.getElementById("regionInput"));
 }
 
 async function fetchTypes(query) {
-    const types = ['Type A', 'Type B', 'Type C']; 
-    const suggestions = types.filter(type => type.toLowerCase().includes(query.toLowerCase()));
+    if (activeInput !== document.getElementById("typeInput") && suggestionList) {
+        suggestionList.style.display = 'none';
+    }
 
-    updateSuggestions('typeSuggestions', suggestions);
+    const types = ['Rouge 1', 'Blanc B', 'Rosé C', 'Rouge 2'];
+    const suggestions = filterSuggestions(types, query);
+
+    updateSuggestions('typeSuggestions', suggestions, query);
+    setActiveInput(document.getElementById("typeInput"));
 }
 
 async function fetchBottleSizes(query) {
-    const bottleSizes = ['750ml', '1L', '1.5L'];
-    const suggestions = bottleSizes.filter(size => size.includes(query));
+    if (activeInput !== document.getElementById("bottleSizeInput") && suggestionList) {
+        suggestionList.style.display = 'none';
+    }
 
-    updateSuggestions('bottleSizeSuggestions', suggestions);
+    const bottleSizes = ['750ml', '1L', '1.5L'];
+    const suggestions = filterSuggestions(bottleSizes, query);
+
+    updateSuggestions('bottleSizeSuggestions', suggestions, query);
+    setActiveInput(document.getElementById("bottleSizeInput"));
 }
 
-function updateSuggestions(datalistId, suggestions) {
-    const datalist = document.getElementById(datalistId);
-    datalist.innerHTML = '';
+function filterSuggestions(allSuggestions, query) {
+    if (!query || query && query === "") {
+        return allSuggestions;
+    }
+    return allSuggestions.filter(data => data.includes(query));
+}
+
+function highlightMatch(suggestion, query) {
+    if (!query) {
+        return suggestion;
+    }
+
+    const startIdx = suggestion.toLowerCase().indexOf(query.toLowerCase());
+    if (startIdx === -1) return suggestion;
+
+    const beforeMatch = suggestion.slice(0, startIdx);
+    const matchText = suggestion.slice(startIdx, startIdx + query.length);
+    const afterMatch = suggestion.slice(startIdx + query.length);
+
+    if (matchText === "") {
+        return afterMatch;
+    }
+
+    return `${beforeMatch}<span class="highlight">${matchText}</span>${afterMatch}`;
+}
+
+function updateSuggestions(datalistId, suggestions, query) {
+    suggestionList = document.getElementById(datalistId);
+    suggestionList.innerHTML = ''; 
+    suggestionList.style.display = suggestions.length > 0 ? 'block' : 'none';
+
     suggestions.forEach(suggestion => {
-        const option = document.createElement('option');
-        option.value = suggestion;
-        datalist.appendChild(option);
+        const suggestionItem = document.createElement('div');
+        suggestionItem.classList.add('suggestion-item');
+
+        const highlightedSuggestion = highlightMatch(suggestion, query);
+
+        suggestionItem.innerHTML = highlightedSuggestion;
+        suggestionItem.addEventListener('click', () => selectSuggestion(suggestion));
+
+        suggestionList.appendChild(suggestionItem);
     });
+}
+
+function selectSuggestion(suggestion) {
+    activeInput.value = suggestion;
+    suggestionList.style.display = 'none';
+}
+
+function setActiveInput(inputElement) {
+    activeInput = inputElement;
+    suggestionList.style.display = 'block';
+    suggestionList.style.width = `${activeInput.offsetWidth}px`;
 }
 
 function submitWineForm(event) {
@@ -105,5 +179,18 @@ function submitWineForm(event) {
     }
 
     console.log("Wine Data Submitted:", wineData);
-    closeModalFunc();
+    closeHardCreationModal();
+}
+
+function toggleMoreFields() {
+    const extraFields = document.getElementById('extraFields');
+    const btn = document.getElementById('toggleMoreFieldsBtn');
+    
+    if (extraFields.style.display === 'none' || extraFields.style.display === '') {
+        extraFields.style.display = 'block';
+        btn.innerHTML = 'Informations supplémentaires &#x2191;';
+    } else {
+        extraFields.style.display = 'none';
+        btn.innerHTML = 'Informations supplémentaires &#x2193;';
+    }
 }
